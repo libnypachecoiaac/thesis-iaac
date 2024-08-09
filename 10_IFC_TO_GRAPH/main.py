7112a55480357096df5f7b4b473998c52039ae3c
chrzbrk <156067510+chrzbrk@users.noreply.github.com>
2024-08-09 12:17:47 +0200

    Update

--- 10_IFC_TO_GRAPH/main.py
+++ 10_IFC_TO_GRAPH/main.py
@@ -6,6 +6,9 @@ from topologicpy.Topology import Topology
 
 from ifcspaces_to_topology import main as ifcspaces_to_topology
 from topology_spaces_to_csv import main as topology_spaces_to_csv
+from doorinfo_to_csv import main as doorinfo_to_csv
+from windowinfo_to_csv import main as windowinfo_to_csv
+from room_bounding_walls_to_csv import main as room_bounding_walls_to_csv
 
 def filter_ifcspaces_by_storey(spaces, storey_name):
     """Filter IfcSpaces by the given storey name."""
@@ -27,6 +30,23 @@ def filter_spaces_by_name(spaces):
             filtered_spaces.append(space)
     return filtered_spaces
 
+def find_ifc_storey(ifc_file, storey_name):
+    """Finding the corresponding IfcBuildingStorey for a given string"""
+    target_storey = None
+    for storey in ifc_file.by_type("IfcBuildingStorey"):
+        if storey.Name == storey_name:
+            target_storey = storey
+            break
+
+    if not target_storey:
+        print(f"Storey with name '{storey_name}' was not found.")
+
+    else:
+        print(f"Storey '{storey_name}' found. OID: {target_storey.id()}")
+        return target_storey
+
+    
+
 def main():
 
     # Load config.yaml
@@ -39,6 +59,9 @@ def main():
     # Open IFC-File
     ifc_file = ifcopenshell.open(ifc_file_path)
     print("IFC file opened successfully.")
+
+    # Find corresponding IfcBuidlingStorey
+    ifc_storey = find_ifc_storey(ifc_file, storey_name)
 
 
 
@@ -67,6 +90,30 @@ def main():
 
     ### Determine adjacent rooms using door informations
 
+    # Find all IfcDoors
+    all_doors = ifc_file.by_type("IfcDoor")
+    # Filter Doors by storey, extract RoomInfos and Write to .csv
+    doorinfo_to_csv(ifc_file, all_doors, ifc_storey)
+
+
+
+    ### Determine adjacent rooms using window informations
+
+    # Find all IfcWindows
+    all_windows = ifc_file.by_type("IfcWindow")
+    # Filter Windows by storey, extract RoomInfos and Write to .csv
+    windowinfo_to_csv(ifc_file,all_windows,ifc_storey)
+    
+
+
+    ### Connectivity between Spaces and Walls
+
+    # Extract adjacent Walls to Rooms from IfcRelation
+    room_bounding_walls_to_csv(ifc_file,ifc_storey, spaces_in_storey)
+
+
+
+    ###
 
 
 
